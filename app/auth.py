16623320d5b724c94466f4583ae0807459811b54
DIprooger <dddddd>
2024-08-30 17:29:47 +0300

    tokenization file

---
+++ app/auth.py
@@ -0,0 +1,70 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from . import user
+from . import models
+from . import schemas
+from typing import Optional
+from dotenv import load_dotenv
+import os
+from . import conf
+
+load_dotenv()
+
+SECRET_KEY: str = os.getenv("SECRET_KEY")
+ALGORITHM: str = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(db: Session, email: str, password: str) -> Optional[models.User]:
+    user_instance = db.query(models.User).filter(models.User.email == email).first()
+    if user_instance and verify_password(password, user_instance.password):
+        return user_instance
+    return None
+
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(conf.get_db)
+) -> schemas.User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+
+    current_user = user.get_user_by_email(db, email=token_data.email)
+    if current_user is None:
+        raise credentials_exception
+    return current_user
